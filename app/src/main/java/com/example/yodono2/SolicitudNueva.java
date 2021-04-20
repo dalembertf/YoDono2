a8ca0e814802d303db0f8274603c9661ada66a96
D'alembert Fernandez <dalembert.f-94@hotmail.com>
2021-04-19 22:50:56 -0300

    Update SolicitudNueva.java

--- app/src/main/java/com/example/yodono2/SolicitudNueva.java
+++ app/src/main/java/com/example/yodono2/SolicitudNueva.java
@@ -2,66 +2,132 @@ package com.example.yodono2;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SolicitudNueva#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.yodono2.Entidades.Donantes;
+import com.example.yodono2.Entidades.Solicitudes;
+
+
 public class SolicitudNueva extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Button boton_solicitar_aceptar;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private YoDonoViewModel yoDonoViewModel;
+
+    DatePicker picker_fecha_limite;
+    EditText text_hospital;
+    EditText text_cantidad_donantes;
+    EditText text_motivo;
+    HorizontalNumberPicker number_cantidad_donantes;
+
+    Donantes donante_logueado;
 
     public SolicitudNueva() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SolicitudNueva.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SolicitudNueva newInstance(String param1, String param2) {
-        SolicitudNueva fragment = new SolicitudNueva();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
+
+        Bundle bd = getActivity().getIntent().getExtras();
+        donante_logueado = (Donantes) bd.get("Donante");
+
+        yoDonoViewModel = new ViewModelProvider( this,
+                ViewModelProvider.AndroidViewModelFactory
+                        .getInstance(this.getActivity().getApplication()))
+                .get(YoDonoViewModel.class);
+
+
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_solicitud_nueva, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        TextView miGrupoSanguineo = (TextView)view.findViewById(R.id.text_grupoSanguineo);
+        miGrupoSanguineo.setText(donante_logueado.getGrupo_Sanguineo());
+
+        number_cantidad_donantes = (HorizontalNumberPicker) view.findViewById(R.id.number_donantes);
+
+        TextView miDepartamento = (TextView)view.findViewById(R.id.text_miDepartamento);
+        miDepartamento.setText(donante_logueado.getDepartamento());
+
+        boton_solicitar_aceptar = (Button)view.findViewById(R.id.boton_solicitar_aceptar);
+
+        boton_solicitar_aceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                text_hospital = (EditText)view.findViewById(R.id.text_hospital);
+                text_motivo = (EditText)view.findViewById(R.id.text_motivo);
+                String hospital = text_hospital.getText().toString();
+                Integer cantidad_int = number_cantidad_donantes.getValue();
+                String cantidad = cantidad_int.toString();
+                String motivo = text_motivo.getText().toString();
+
+                String cedula = donante_logueado.getCedula();
+                String nombre = donante_logueado.getNombre();
+                String apellido = donante_logueado.getApellido();
+                String departamento = donante_logueado.getDepartamento();
+                String grupo_sanguineo = donante_logueado.getGrupo_Sanguineo();
+
+                picker_fecha_limite = (DatePicker)view.findViewById(R.id.fecha_limite);
+                Integer mes_i, dia_i, anio_i;
+                String mes, dia, anio, fecha;
+                mes_i = picker_fecha_limite.getMonth();
+                mes = mes_i.toString();
+                if ( mes_i < 10 ) { mes = '0' + mes; }
+                dia_i = picker_fecha_limite.getDayOfMonth();
+                dia = dia_i.toString();
+                if ( dia_i < 10 ) { dia = '0' + dia; }
+                anio_i = picker_fecha_limite.getYear();
+                anio = anio_i.toString();
+
+                fecha = anio + mes + dia;
+
+                if (nombre.isEmpty() || apellido.isEmpty() || cedula.isEmpty() || fecha.isEmpty() || hospital.isEmpty() || cantidad.isEmpty() || motivo.isEmpty() )
+                {
+                    Toast.makeText(getActivity(), "Debe completar todos los datos", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+
+                    Solicitudes nueva_solicitud = new Solicitudes( cedula, nombre, apellido, grupo_sanguineo, hospital, fecha, motivo, cantidad, departamento );
+
+                    yoDonoViewModel.insert( nueva_solicitud );
+
+                    Toast.makeText(getActivity(), "Solicitud Agregada", Toast.LENGTH_SHORT).show();
+
+
+                }
+
+            }
+        });
     }
 }
