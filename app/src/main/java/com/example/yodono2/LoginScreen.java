9308f6297990879c887ef99a0e3a29e3d4c2fe8d
Shadowpride <60741137+Shadowpride@users.noreply.github.com>
2021-04-20 12:49:51 -0300

    Login notification

--- app/src/main/java/com/example/yodono2/LoginScreen.java
+++ app/src/main/java/com/example/yodono2/LoginScreen.java
@@ -1,6 +1,9 @@
 package com.example.yodono2;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +11,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.ui.AppBarConfiguration;
 
@@ -24,6 +29,21 @@ public class LoginScreen extends AppCompatActivity {
 
     private YoDonoViewModel yoDonoViewModel;
 
+    private int  CHANNEL_ID = 1;
+    private String  channelID = "channelID";
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(channelID, name, importance);
+            channel.setDescription(description);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +53,17 @@ public class LoginScreen extends AppCompatActivity {
                 ViewModelProvider.AndroidViewModelFactory
                         .getInstance(this.getApplication()))
                 .get(YoDonoViewModel.class);
+
+        createNotificationChannel();
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, channelID)
+                .setSmallIcon(R.drawable.yodono_favicon)
+                .setContentTitle("Notificacion de Donante")
+                .setContentText("")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Usted tiene una donacion pendiente"))
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
 
 
         //dataBase = AppDatabase.getInstance( LoginScreen.this );
@@ -53,6 +84,7 @@ public class LoginScreen extends AppCompatActivity {
                 Donantes donante = yoDonoViewModel.buscarDonante( cedula, contrasena );
                 if ( donante != null )
                 {
+                    notificationManager.notify(CHANNEL_ID, builder.build());
                     Intent i = new Intent(LoginScreen.this, MainActivity.class);
                     i.putExtra( "Donante", donante );
                     startActivity(i);
