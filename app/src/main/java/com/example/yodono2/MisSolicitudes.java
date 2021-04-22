c88b5414fc10d5efc3262a34fd0c42e4883f9783
D'alembert Fernandez <dalembert.f-94@hotmail.com>
2021-04-22 00:43:23 -0300

    Mis solicitudes

--- app/src/main/java/com/example/yodono2/MisSolicitudes.java
+++ app/src/main/java/com/example/yodono2/MisSolicitudes.java
@@ -1,64 +1,80 @@
 package com.example.yodono2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MisSolicitudes#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MisSolicitudes extends Fragment {
+import com.example.yodono2.Entidades.Donantes;
+import com.example.yodono2.Entidades.Solicitudes;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.List;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public MisSolicitudes() {
-        // Required empty public constructor
+public class MisSolicitudes extends Fragment implements SolicitudesAdapter.OnSolicitudListener {
+
+    Intent intent;
+    SolicitudesAdapter adapter;
+    private YoDonoViewModel yoDonoViewModel;
+
+    private RecyclerView recyclerView;
+    private List<Solicitudes> lista_solicitudes;
+    Donantes donante_logueado;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView = view.findViewById(R.id.ListRecyclerViewSolicitudes);
+        //recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        adapter = new SolicitudesAdapter( this );
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+
+        intent = getActivity().getIntent();
+        Bundle bd = intent.getExtras();
+
+        donante_logueado = (Donantes)bd.get("Donante");
+
+        yoDonoViewModel = new ViewModelProvider(this,
+                ViewModelProvider.AndroidViewModelFactory
+                        .getInstance(this.getActivity().getApplication()))
+                .get(YoDonoViewModel.class);
+
+        actualizar_lista_solicitudes();
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MisSolicitudes.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MisSolicitudes newInstance(String param1, String param2) {
-        MisSolicitudes fragment = new MisSolicitudes();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public void actualizar_lista_solicitudes() {
+        yoDonoViewModel.getSolicitudesDonante( donante_logueado.getCedula() ).observe(getViewLifecycleOwner(), new Observer<List<Solicitudes>>() {
+            @Override
+            public void onChanged(List<Solicitudes> solicitudes) {
+                lista_solicitudes = solicitudes;
+                adapter.setSolicitudes( solicitudes );
+            }
+        });
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mis_solicitudes, container, false);
+    public void onSolicitudClick(int posicion) {
+        Solicitudes solicitud_clickeada = lista_solicitudes.get( posicion );
+        Intent intent = new Intent( this.getContext(), SolicitudIndividual.class);
+        intent.putExtra( "Solicitud", solicitud_clickeada );
+        intent.putExtra( "Donante", donante_logueado );
+        startActivity( intent );
     }
 }
